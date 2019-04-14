1733e0985f790183e8c443e3032bb952c51e6744
teguh01 <45221984+teguh01@users.noreply.github.com>
2019-04-14 14:37:49 +0700

    Update and rename opencvHaar.py to OpenCVHaar.py

--- OpenCVHaar.py
+++ OpenCVHaar.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
  
 image = cv2.imread('gambar.jpg')
 grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
